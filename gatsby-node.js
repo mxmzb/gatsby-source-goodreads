f64fe66b81936831dc6c914c77f1df8bae7d7e1a
Gavin Mogan <git@gavinmogan.com>
2018-05-24 11:35:43 -0700

    Testing is cool yo

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,59 @@
 const axios = require(`axios`);
 const crypto = require(`crypto`);
-const parseString = require('pify')(require("xml2js").parseString);
+const parseString = require('pify')(require('xml2js').parseString);
 
+function processBook (reviewElement) {
+  const bookElement = reviewElement.book[0];
+
+  let isbnValue = bookElement.isbn[0];
+  let isbn13Value = bookElement.isbn13[0];
+  if (isNaN(isbnValue)) {
+    isbnValue = null;
+  }
+  if (isNaN(isbn13Value)) {
+    isbn13Value = null;
+  }
+  const shelfNames = reviewElement.shelves[0].shelf.map(s => s.$.name);
+  const review = {
+    reviewID: reviewElement.id[0],
+    rating: reviewElement.rating[0],
+    votes: reviewElement.votes[0],
+    spoilerFlag: reviewElement.spoiler_flag[0],
+    spoilersState: reviewElement.spoilers_state[0],
+    dateAdded: reviewElement.date_added[0],
+    dateUpdated: reviewElement.spoilers_state[0],
+    startedAt: reviewElement.started_at[0],
+    readAt: reviewElement.read_at[0]
+  };
+  const book = {
+    bookID: bookElement.id[0]._,
+    isbn: isbnValue,
+    isbn13: isbn13Value,
+    textReviewsCount: bookElement.text_reviews_count[0]._,
+    uri: bookElement.uri[0],
+    link: bookElement.link[0],
+    title: bookElement.title[0],
+    titleWithoutSeries: bookElement.title_without_series[0],
+    imageUrl: bookElement.image_url[0],
+    smallImageUrl: bookElement.small_image_url[0],
+    largeImageUrl: bookElement.large_image_url[0],
+    description: bookElement.description[0],
+    authors: bookElement.authors[0].author.map(authorElement => ({
+      id: authorElement.id[0],
+      name: authorElement.name[0],
+      imageUrl: (authorElement.image_url[0]._ || '').trim(),
+      smallImageUrl: (authorElement.small_image_url[0]._ || '').trim(),
+      link: authorElement.link[0],
+      average_rating: authorElement.average_rating[0],
+      ratings_count: authorElement.ratings_count[0],
+      text_reviews_count: authorElement.text_reviews_count[0]
+    }))
+  };
+  return { shelfNames, book, review };
+}
 exports.sourceNodes = async (
   { boundActionCreators, reporter },
-  { goodReadsUserId, userShelf = "", developerKey = "" }
+  { goodReadsUserId, userShelf = '', developerKey = '' }
 ) => {
   const { createNode } = boundActionCreators;
   if (!goodReadsUserId) {
@@ -34,40 +83,7 @@ exports.sourceNodes = async (
       const end = parseInt(result.GoodreadsResponse.reviews[0].$.end);
       const total = parseInt(result.GoodreadsResponse.reviews[0].$.total);
       for (const element of result.GoodreadsResponse.reviews[0].review) {
-        const bookElement = element.book[0];
-
-        let isbnValue = bookElement.isbn[0];
-        let isbn13Value = bookElement.isbn13[0];
-        if (isNaN(isbnValue)) {
-          isbnValue = null;
-        }
-        if (isNaN(isbn13Value)) {
-          isbn13Value = null;
-        }
-        const shelfNames = element.shelves[0].shelf.map(s => s.$.name);
-        const review = {
-          reviewID: element.id[0],
-          rating: element.rating[0],
-          votes: element.votes[0],
-          spoilerFlag: element.spoiler_flag[0],
-          spoilersState: element.spoilers_state[0],
-          dateAdded: element.date_added[0],
-          dateUpdated: element.spoilers_state[0],
-        }
-        const book = {
-          bookID: bookElement.id[0]._,
-          isbn: isbnValue,
-          isbn13: isbn13Value,
-          textReviewsCount: bookElement.text_reviews_count[0]._,
-          uri: bookElement.uri[0],
-          link: bookElement.link[0],
-          title: bookElement.title[0],
-          titleWithoutSeries: bookElement.title_without_series[0],
-          imageUrl: bookElement.image_url[0],
-          smallImageUrl: bookElement.small_image_url[0],
-          largeImageUrl: bookElement.large_image_url[0],
-          description: bookElement.description[0]
-        }
+        const { shelfNames, review, book } = processBook(element);
         createNode({
           id: review.reviewID,
           shelfNames,
@@ -79,15 +95,15 @@ exports.sourceNodes = async (
           internal: {
             type: `GoodreadsBook`,
             contentDigest: crypto
-            .createHash(`md5`)
-            .update(`book${goodReadsUserId}`)
-            .digest(`hex`)
+              .createHash(`md5`)
+              .update(`book${goodReadsUserId}`)
+              .digest(`hex`)
           }
         });
       }
       page++;
-      if (end >= total) { 
-        break; 
+      if (end >= total) {
+        break;
       }
     }
   } catch (err) {
